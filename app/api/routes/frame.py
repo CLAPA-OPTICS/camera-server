9cf54f1b432476813dbc3b44573824939087e2e4
Jie Cai <jiecai@stu.pku.edu.cn>
2023-07-07 13:14:02 +0800

    devlopment v0.0.1

---
+++ app/api/routes/frame.py
@@ -0,0 +1,102 @@
+import cv2
+import asyncio
+import numpy as np
+from typing import Optional
+from pydantic import BaseModel
+from fastapi import APIRouter, Body, Depends, Response, BackgroundTasks
+from fastapi.responses import StreamingResponse, ORJSONResponse
+from .Camera import Camera
+
+isAddFrame = False
+
+def get_camera():
+    camera = Camera()
+    return camera
+
+def get_queue():
+    return asyncio.Queue(20)
+
+def add_frame(q: asyncio.Queue, camera: Camera):
+    while isAddFrame:
+        q.put(camera.get_frame())
+    return
+
+class Item(BaseModel):
+    name: str
+    min: float
+    max: float
+
+async def generate_frames(q: asyncio.Queue = Depends(get_queue),
+                          type = "8_bits"):
+    global isAddFrame
+    while isAddFrame:
+        # 从视频流中读取帧。
+        frame = cv2.cvtColor(q.get(), cv2.COLOR_GRAY2BGR)
+        # 将帧转换为JPEG格式。
+        ret, buffer = cv2.imencode(".jpg", frame)
+        # 将JPEG数据转换为字节字符串，并将其作为流响应返回。
+        yield (b"--frame\r\n"
+               b"Content-Type: image/jpeg\r\n\r\n" + buffer.tobytes() + b"\r\n")
+        
+def get_axis_data(camera: Camera = Depends(get_camera), 
+                  x1: float = 0.0, x2: float = 100.0):
+    data = camera.raw_frame()
+    shape_1 = data.shape[1]
+    index_1 = np.arange(shape_1)
+    
+    data_1 = np.sum(data[int(x1/100*shape_1 - 1):int(x2/100*shape_1 - 1)], axis = 0)
+    data_1 = data_1/np.nanmax(data_1)
+    return [{"index_0": index_1[i], "axis_0": data_1[i]} for i in range(index_1.shape[0])]
+
+def set_exposure_time(camera: Camera = Depends(get_camera), 
+                      time: float = 200000):
+    camera.CameraSetExposureTime(time)
+    return
+
+router = APIRouter()
+
+@router.get("/frame")
+async def get_8_bits_img(type: str = "8_bits"):
+    """将视频流作为流响应返回。"""
+    data = generate_frames(type)
+    return StreamingResponse(data,
+                    media_type="multipart/x-mixed-replace;boundary=frame")
+
+# 一维投影。
+@router.get("/get_projection", response_class=ORJSONResponse)
+def get_projection(x1: float = 0.0, x2: float = 100.0):
+    """将视频流作为流响应返回。"""
+    data = get_axis_data(x1, x2)
+    return ORJSONResponse(data)
+
+# 设置相机曝光。
+@router.post("/set_exposure_time", response_class=ORJSONResponse)
+def get_projection(time: float = 0.0):
+    """将视频流作为流响应返回。"""
+    set_exposure_time(time)
+    return
+
+@router.get("/start")
+def start(camera: Camera = Depends(get_camera)):
+    camera.start()
+    return
+
+@router.get("/close")
+def close(camera: Camera = Depends(get_camera)):
+    camera.release()
+    return
+
+@router.get("/start_sample")
+def start_sample(backgroundtasks: BackgroundTasks, 
+                 q: asyncio.Queue = Depends(get_queue),
+                 camera: Camera = Depends(get_camera)):
+    global isAddFrame
+    isAddFrame = True
+    backgroundtasks.add_task(add_frame, q, camera)
+    return
+
+@router.get("/stop_sample")
+def stop_sample():
+    global isAddFrame
+    isAddFrame = False
+    return
