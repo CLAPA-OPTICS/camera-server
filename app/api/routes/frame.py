f3ba33dfb2c8abefef120d3b866854c6e8847451
Jie Cai <jiecai@pku.stu.edu.com>
2023-07-11 12:55:56 +0800

    dev 0.1.0

--- app/api/routes/frame.py
+++ app/api/routes/frame.py
@@ -13,7 +13,12 @@ from fastapi import APIRouter, Depends
 from fastapi.responses import StreamingResponse, ORJSONResponse
 from app.core.Camera import Camera
 
-isAddFrame = False
+FWHM: list[int] = [0, 0, 0, 0]
+isAddFrame: bool = False
+isCrossLine: bool = False
+left_bound: float = 0.0
+right_bound: float = 100.0
+axis_list: list[dict] = []
 
 def get_camera(request: Request) -> Camera:
     application = request.app
@@ -30,14 +35,22 @@ def add_frame(q: asyncio.Queue, camera: Camera):
 
 async def generate_frames(camera: Camera,
                           type = "8_bits",
-                          cmap: int = -1):
+                          cmap: int = -1) -> bytes:
     global isAddFrame
     while True:
         # 从视频流中读取帧。
         
         if not isAddFrame:
             break
+
         data = camera.get_frame() if type == '8_bits' else camera.raw_frame()
+
+        if type == "12_bits":
+            data = data >> 4
+
+        global left_bound, right_bound
+        calcu_axis_data(data, left_bound, right_bound)
+
         frame = cv2.cvtColor(data, cv2.COLOR_GRAY2BGR)
         if cmap >=0:
             frame = cv2.applyColorMap(frame, cmap)
@@ -47,34 +60,51 @@ async def generate_frames(camera: Camera,
         yield (b"--frame\r\n"
                b"Content-Type: image/jpeg\r\n\r\n" + buffer.tobytes() + b"\r\n")
         
-def get_axis_data(camera: Camera, 
-                  x1: float = 0.0, x2: float = 100.0):
-    
-    data = camera.raw_frame()
+def calcu_axis_data(data: np.ndarray, 
+                  x1: float = 0.0, x2: float = 100.0) -> list[dict]:
+
     shape_1 = data.shape[1]
     index_1 = np.arange(shape_1)
-    print(int(x1/100*(shape_1 - 1)), int(x2/100*(shape_1 - 1)))
-    data_1 = np.sum(data[int(x1/100*(shape_1 - 1)):int(x2/100*(shape_1 - 1))], axis = 0)
-    data_1 = data_1/np.nanmax(data_1)
-    return [{"index_0": index_1[i], "axis_0": data_1[i]} for i in range(index_1.shape[0])]
 
-def get_fwhm(camera: Camera, 
-             x1: float = 0.0, x2: float = 100.0):
-    
-    data = camera.raw_frame()
-    shape_1 = data.shape[1]
-    print(int(x1/100*(shape_1 - 1)), int(x2/100*(shape_1 - 1)))
     data_1 = np.sum(data[int(x1/100*(shape_1 - 1)):int(x2/100*(shape_1 - 1))], axis = 0).flatten()
     data_1 = data_1/np.nanmax(data_1)
-    logger.info(data_1)
+    global axis_list 
+    axis_list = [{"index_0": index_1[i], "axis_0": data_1[i]} for i in range(index_1.shape[0])]
+
     peaks, _ = find_peaks(data_1, rel_height = 1.0)
     results_half = peak_widths(data_1, peaks, rel_height = 0.5)
+    global FWHM
+    FWHM = results_half
+    return
+
+def get_axis_data() -> list:
+    global axis_list
+    global FWHM
+    return [axis_list, 
+            {"width": FWHM[0], 
+            "height": FWHM[1], 
+            "x1": FWHM[2], 
+            "x2": FWHM[3]}]
+
+def get_fwhm() -> list[int]:
+    
+    global FWHM
+    results_half = FWHM
 
     return {"width": results_half[0], 
             "height": results_half[1], 
             "x1": results_half[2], 
             "x2": results_half[3]}
 
+'''
+def calculate_fwhm(data: np.ndarray, x1: float = 0.0, x2: float = 100.0) -> list[int]:
+    shape_1 = data.shape[1]
+    data_1 = np.sum(data[int(x1/100*(shape_1 - 1)):int(x2/100*(shape_1 - 1))], axis = 0).flatten()
+    data_1 = data_1/np.nanmax(data_1)
+    peaks, _ = find_peaks(data_1, rel_height = 1.0)
+    results_half = peak_widths(data_1, peaks, rel_height = 0.5)
+    return results_half
+'''
 
 '''
 def set_exposure_time(camera: Camera, 
@@ -84,6 +114,7 @@ def set_exposure_time(camera: Camera,
     camera.play()
     return
 '''
+
 
 def set_cross_line(camera: Camera, 
                    x: int = 0, 
@@ -134,17 +165,17 @@ async def get_8_bits_img(camera: Camera = Depends(get_camera),
 @router.get("/get_projection", response_class=ORJSONResponse)
 def get_projection(camera: Camera = Depends(get_camera), 
                    x1: float = 0.0, x2: float = 100.0):
-
-    data = get_axis_data(camera, x1, x2)
+    global left_bound, right_bound
+    left_bound, right_bound = x1, x2
+    data = get_axis_data()
     return ORJSONResponse(data)
 
 
 # 一维投影。
 @router.get("/get_fwhm", response_class=ORJSONResponse)
-def get_fwhm_value(camera: Camera = Depends(get_camera), 
-                   x1: float = 0.0, x2: float = 100.0):
+def get_fwhm_value():
 
-    data = get_fwhm(camera, x1, x2)
+    data = get_fwhm()
 
     return ORJSONResponse(data)
 
